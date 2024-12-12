7e2dfa5c88d4878e1aba8ee67309e181d7fc3137
nuyhhyun <sarahs19991115@gmail.com>
2024-12-12 16:50:40 +0900

    HourlyForecast 컴포넌트 완성

--- alom-08/src/components/HourlyForecast.js
+++ alom-08/src/components/HourlyForecast.js
@@ -4,8 +4,29 @@ import { getWeatherDescription, formatHourlyData } from "../utils/weather";
 
 const HourlyForecast = ({ weatherData }) => {
   const hourlyData = formatHourlyData(weatherData);
+  const hourlyFormat = hourlyData.time.map((dateString) => {
+    const date = new Date(dateString);
+    const hours = date.getHours();
+    return `${hours}시`;
+  });
 
-  return <div>채워주세요</div>;
+  const hourlyForecast = hourlyData.time.slice(0, 24).map((_, index) => [
+    hourlyFormat[index],
+    hourlyData.temperature_2m[index] + "°C",
+    getWeatherDescription(hourlyData.weather_code[index]),
+  ]);
+
+  return (
+    <HourlyForecastWrapper>
+      {hourlyForecast.map((row, rowIndex) => (
+        <HourlyItem key={rowIndex}>
+          <div>{row[0]}</div>
+          <div>{row[1]}</div>
+          <div>{row[2]}</div>
+        </HourlyItem>
+      ))}
+    </HourlyForecastWrapper>
+  );
 };
 
 export default HourlyForecast;
