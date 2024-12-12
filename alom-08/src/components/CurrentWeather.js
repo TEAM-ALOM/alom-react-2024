6d0276d4d7547ed07456f41e445c612409d29597
nuyhhyun <sarahs19991115@gmail.com>
2024-12-12 17:08:25 +0900

    CurrentWeather 컴포넌트 완성

--- alom-08/src/components/CurrentWeather.js
+++ alom-08/src/components/CurrentWeather.js
@@ -3,15 +3,38 @@ import {
   CurrentWeatherWrapper,
   Temperature,
   WeatherCode,
+  LoadingWrapper,
+  LoadingText,
 } from "./styles/StyledComponents";
-import { getWeatherDescription } from "../utils/weather";
+import { getWeatherDescription, formatHourlyData } from "../utils/weather";
 
 const CurrentWeather = ({ weatherData, isLoading }) => {
   if (isLoading) {
-    return <div>채워주세요</div>;
+    return (
+      <LoadingWrapper>
+        <LoadingText> 로딩 중입니당.. 잠시만용ㅠ </LoadingText>
+      </LoadingWrapper>
+    );
   }
+  const currentData = formatHourlyData(weatherData);
+  const currentTime = new Date();
+  const closestIndex = currentData.time.findIndex(time => {
+    const parsedTime = new Date(time);
+    return parsedTime <= currentTime;
+  });  
 
-  return <div>채워주세요</div>;
+  return (
+    <div>
+      <CurrentWeatherWrapper>
+        <Temperature>
+          {currentData.temperature_2m[closestIndex]}°C
+        </Temperature>
+        <WeatherCode>
+          {getWeatherDescription(currentData.weather_code[closestIndex])}
+        </WeatherCode>
+      </CurrentWeatherWrapper>
+    </div>
+  );
 };
 
 export default CurrentWeather;
